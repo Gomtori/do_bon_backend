8327ccfe887bcf6d093d16f6b0fc1fc33b7c239d
wj <nawonjae@naver.com>
2023-01-11 20:41:46 +0900

    initial commit

---
+++ src/main/java/com/gomtori/do_bon/DoBonApplication.java
@@ -0,0 +1,13 @@
+package com.gomtori.do_bon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoBonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoBonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomtori/do_bon/DoBonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomtori.do_bon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoBonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
